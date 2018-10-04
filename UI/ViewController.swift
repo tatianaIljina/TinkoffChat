a638c0485695b0f73b0b5546448385d7a00bda4c
Unknown <ilyitatjana@yandex.ru>
2018-10-04 10:00:40 +0300

    Add openCamera() and openGallery()

--- UI/ViewController.swift
+++ UI/ViewController.swift
@@ -7,64 +7,53 @@
 //
 
 import UIKit
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate  {
+class ViewController: UIViewController {
 
     @IBOutlet weak var editButton: UIButton!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var profileImage: UIImageView!
     @IBOutlet weak var loadPhoto: UIButton!
+    let imagePicker = UIImagePickerController()
     @IBAction func loadPhotoAction(_ sender: Any) {
-        let sheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        present(sheet, animated: true, completion: nil)
-        let importPhoto = UIAlertAction(title: "Установить из галлереи", style: .destructive) { (action) in
-            let image = UIImagePickerController()
-            image.delegate = self
-   
-            image.sourceType = UIImagePickerController.SourceType.photoLibrary
-            image.allowsEditing = false
-            self.present(image, animated: true)
-            {
-                print("Фотография выбрана")
-
-            }
-            func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
-                self.dismiss(animated: true, completion: nil)
-            }
-            func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
-                guard let selectedImage = UIImagePickerController.InfoKey.originalImage as? UIImage else {
-                    fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")}
-                self.profileImage.image = selectedImage
-                self.dismiss(animated: true, completion: nil)
-            }
-            
-//            func imagePickerController (_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-//                if let selectedImage = UIImagePickerController.InfoKey.originalImage as? UIImage  {
-//                    self.profileImage.image = selectedImage
-//                }
-//                else{
-//                    print("ERROR!!!")
-//                }
-//                self.dismiss(animated: true, completion: nil)
-//            }
-            
-        }
-        let makePhoto = UIAlertAction(title: "Сделать фото", style: .destructive) { (action) in
-            if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
-                let image = UIImagePickerController()
-                image.delegate = self
-                image.sourceType = UIImagePickerController.SourceType.camera
-                image.allowsEditing = false
-                self.present(image, animated: true)
-                {
-                    print("Фотография выбрана")
-                
-                }
-                
-            }
-        }
+        let sheet = UIAlertController(title: "Add photo", message: nil, preferredStyle: .actionSheet)
+        let importPhoto = UIAlertAction(title: "Установить из галлереи", style: .destructive) { (action) in self.openGallery()}
+        let makePhoto = UIAlertAction(title: "Сделать снимок", style: .destructive) { (action) in self.openCamera()}
+        
         sheet.addAction(importPhoto)
         sheet.addAction(makePhoto)
+        
         print ("Выберите изображение профиля")
+        
+        self.present(sheet, animated: true)
+        
+    }
+
+    func openGallery(){
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary;
+            imagePicker.allowsEditing = false
+            present(imagePicker, animated: true, completion: nil)
+        }
+        else {
+            let warning = UIAlertController(title: "Не могу открыть галерею", message: nil, preferredStyle: .alert)
+            warning.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(warning, animated: true)
+    }
+    }
+    
+    func openCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera){
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerController.SourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true)
+        }
+        else {
+            let warning = UIAlertController(title: "Не могу открыть камеру", message: nil, preferredStyle: .alert)
+            warning.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(warning, animated: true)
+        }
     }
     
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)   {
@@ -99,9 +88,26 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         let frameVDA = editButton.frame
         print(frameVDA) //значения разные поскольку метод viewDidLoad берет размеры из storyboard, а viewDidAppear размеры уже реального устройства
     }
-    
-    
-    
-    
 }
+
+    
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+            else {
+                fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+            self.profileImage.image = selectedImage
+        
+            self.dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+    
+    
+
 
